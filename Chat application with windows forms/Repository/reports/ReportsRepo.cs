9bbfaa3dad3e11ea2f217e08d4ae16b7419fc06e
joanjanku2000 <janku.joan4@gmail.com>
2022-01-30 01:29:05 +0100

    Reports repo and stored procedures

---
+++ Chat application with windows forms/Repository/reports/ReportsRepo.cs
@@ -0,0 +1,108 @@
+﻿using Chat_application_with_windows_forms.Entities;
+using Chat_application_with_windows_forms.Repository.user;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_application_with_windows_forms.Repository.reports
+{
+    public class ReportsRepo
+    {
+
+        private SqlConnection conn;
+        private UserRepo userRepo;
+
+        private string SENT_MOST_MESSAGES_PROC = "SENT_MOST_MESSAGES";
+        private string GROUPS_MOST_INTERACTED_WITH = "GROUPS_MOST_INTERACTED_WITH";
+        private string RECEIVED_MOST_MESSAGES_PROC = "RECEIVED_MOST_MESSAGES";
+        public ReportsRepo()
+        {
+            conn = DatabaseConnection.getInstance();
+            userRepo = new UserRepo();
+        }
+
+        public Dictionary<long,long> sentMostMessagesTo(long userid)
+        {
+            conn.Open();
+
+            SqlCommand sqlCommand = conn.CreateCommand();
+            sqlCommand.CommandText = SENT_MOST_MESSAGES_PROC;
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@Userid", userid);
+
+            Dictionary<long, long> toReturn = new Dictionary<long, long>();
+
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    long receiverid = reader.GetInt64(0);
+                    long totalMessagesSent = reader.GetInt64(1);
+
+                    toReturn.Add(receiverid, totalMessagesSent);
+                }
+            }
+
+            conn.Close();
+
+            return toReturn;
+        }
+
+        public Dictionary<long, long> receivedMostMessagesFrom(long userid)
+        {
+            conn.Open();
+
+            SqlCommand sqlCommand = conn.CreateCommand();
+            sqlCommand.CommandText = RECEIVED_MOST_MESSAGES_PROC\;
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@Userid", userid);
+
+            Dictionary<long, long> toReturn = new Dictionary<long, long>();
+
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    long senderId = reader.GetInt64(0);
+                    long totalMessagesSent = reader.GetInt64(1);
+
+                    toReturn.Add(senderId, totalMessagesSent);
+                }
+            }
+
+            conn.Close();
+
+            return toReturn;
+        }
+
+        public Dictionary<long, long> groupsWithMostInteraction(long userid)
+        {
+            conn.Open();
+
+            SqlCommand sqlCommand = conn.CreateCommand();
+            sqlCommand.CommandText = GROUPS_MOST_INTERACTED_WITH;
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@Userid", userid);
+
+            Dictionary<long, long> toReturn = new Dictionary<long, long>();
+
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    long groupid = reader.GetInt64(0);
+                    long totalMessagesSent = reader.GetInt64(1);
+
+                    toReturn.Add(groupid, totalMessagesSent);
+                }
+            }
+
+            conn.Close();
+
+            return toReturn;
+        }
+    }
+}
---
+++ Chat application with windows forms/Utils/ReportsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat_application_with_windows_forms.Utils
+{
+    public partial class ReportsForm : Form
+    {
+        public ReportsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
